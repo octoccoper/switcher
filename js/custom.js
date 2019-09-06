36efa4f7b88b1af6ad01d640bba5cd27b1b6609c
octoccoper <octocopper@gmail.com>
2019-09-06 14:59:28 +0300

    added checkSwitcherState()

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,33 @@
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    var switcher = document.querySelectorAll("[data-type='c-switcher'] [type='checkbox']")[0],
-    eventSwitcher = new CustomEvent('switcher.change', { detail: { status: switcher.checked }});
-
-    switcher.addEventListener('switcher.change',switcherChangeFunc, false);
-    switcher.dispatchEvent(eventSwitcher);
+    var switcher = document.querySelectorAll("[data-type='c-switcher'] [type='checkbox']")[0];
+    switcher.addEventListener("change",checkSwitcherState);
 
 });
 
 function switcherChangeFunc(e) {
+
     var switcherState;
     switcherState = e.detail.status;
-    console.log("switcherChange works!", "switcherState is: ",switcherState);
+    if(switcherState) {
+        console.log("switcherChangeFunc works!", "switcherState is: ",switcherState);
+    } else {
+        console.log("switcherChangeFunc works!", "switcherState is: ",switcherState);
+    }
+
+}
+
+function checkSwitcherState() {
+
+    var checkboxSwitcher = document.querySelectorAll("[data-type='c-switcher'] [type='checkbox']")[0],
+        eventSwitcher = new CustomEvent('switcher.change', { detail: { status: checkboxSwitcher.checked }});
+    if (checkboxSwitcher.checked === true) {
+        checkboxSwitcher.addEventListener('switcher.change',switcherChangeFunc, true);
+        checkboxSwitcher.dispatchEvent(eventSwitcher);
+        console.log("checkboxSwitcher.checked === true");
+    } else {
+        console.log("checkboxSwitcher.checked === false");
+    }
+
 }
